5c4f05c1e131a2815d0e337bf04c336728c440b9
Raphaël SEBAN <motus@laposte.net>
2014-10-18 07:46:05 +0200

    preparing for PyPI

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@
 from distutils.core import setup
 
 setup(
-    name = "tk2048",
-    version = "1.1",
-    requires = ["Tkinter"],
-    description = "tk2048 - Python3-Tkinter port of Gabriele Cirulli's 2048 famous Game",
-    author = "Raphaël SEBAN",
-    author_email = "motus@laposte.net",
-    maintainer = "Raphaël SEBAN",
-    maintainer_email = "motus@laposte.net",
-    url = "https://github.com/tarball69/tk2048",
-    download_url = "https://github.com/tarball69/tk2048",
-    keywords = ["tkinter", "game", "2048"],
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
+    name="tkBoulderDash",
+    version="0.36b",
+    requires=["Tkinter"],
+    description="tkBoulderDash - a Python3/Tkinter port of the famous game",
+    author="Raphaël SEBAN",
+    author_email="motus@laposte.net",
+    maintainer="Raphaël SEBAN",
+    maintainer_email="motus@laposte.net",
+    url="https://github.com/tarball69/tkBoulderDash",
+    download_url="https://github.com/tarball69/tkBoulderDash/releases",
+    keywords=["tkinter", "game", "boulderdash"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Environment :: Win32 (MS Windows)",
         "Environment :: X11 Applications",
@@ -38,17 +38,20 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Topic :: Games/Entertainment",
-        "Topic :: Games/Entertainment :: Board Games",
+        "Topic :: Games/Entertainment :: Arcade",
         "Topic :: Games/Entertainment :: Puzzle Games",
     ],
-    license = """
+    license="""
 Licensed under GNU General Public License v3.
     """,
-    long_description = """
+    long_description="""
 
-tk2048 - Python3-Tkinter port of Gabriele Cirulli's 2048 famous Game.
+**tkBoulderDash** is a simple Python3/Tkinter port of **First Star
+Software** famous **Boulder Dash&trade;** 1980s retro game.
 
-This game is a **freefullware** (see below).
+This game is a **freefullware** (see [below](#freefullware)).
+
+Please, feel free to visit [project's website] (http://tarball69.github.io/tkBoulderDash/).
 
 Copyright
 ---------
@@ -60,17 +63,30 @@ License
 
 This software is licensed under **GNU GPL General Public License v3**.
 
-New in v1.1
--------------
-
-* added new high-score feature;
-
-* added new 'New Game' button;
-
-* added new GridAnimation class;
-
 Quick start
 -----------
+
+Notice
+------
+
+This software runs only with **Python3** and **Tkinter** installed
+on your machine.
+
+**No dependencies**, no third-part lib to install on more.
+
+If you have Python3 correctly installed, Tkinter library should also
+be installed **by default** as a Python standard lib.
+
+Any **ImportError** will mean either you are trying to launch the
+game with Python2 or you don't have **Tkinter** library correctly
+installed on your system.
+
+Installing a Python3 version of the language **does not alter** an
+already installed Python2 version in any way.
+
+You may consider installing Python3 from:
+
+https://www.python.org/downloads/ (Ctrl+click: open in new tab)
 
 MS-Windows users
 ----------------
@@ -110,13 +126,13 @@ Bug report
 In order to **track bugs** and fix them correctly, we'd like to hear
 from you.
 
-**If you encountered any problem** during the use of `tk2048`,
+**If you encountered any problem** during the use of this game,
 please leave us a comment and tell us:
 
 * environment:
     * which platform? (Windows, macOS, Linux)
     * which Python version? (2.7+, 3.2+)
-    * which tk2048 version?
+    * which game release version?
     * tkinter installed correctly? (yes/no)
 
 * traceback (optional):
@@ -125,11 +141,11 @@ please leave us a comment and tell us:
 
 **Whatever happened**, we'd like to know about it.
 
-You will find an open issue **"It did *NOT* work for me!"** at:
+You will find a bug tracker service where you can post your issues at:
 
-https://github.com/tarball69/tk2048/issues/1
+https://github.com/tarball69/tkBoulderDash/issues
 
-**Thank you for contributing** to make `tk2048` a cool game for
+**Thank you for contributing** to make this project a cool game for
 everyone.
 
 """
